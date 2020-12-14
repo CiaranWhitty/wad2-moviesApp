4795b7a53ef9d70e1bc4c9add63c202ab65924e0
Ciaran Whitty <whittyciaran1998@gmail.com>
2020-12-14 20:00:12 +0000

    Agile - Redoing Cypress Tests to fit Web Dev Redesign

--- cypress/integration/movieDetails-page.spec.js
+++ cypress/integration/movieDetails-page.spec.js
@@ -33,31 +33,28 @@ describe("Movie Details Page", () => {
   });
 
   it("should display movie title in the page header", () => {
-    cy.get("h2").contains(movie.title);
+    cy.get("h1").contains(movie.title);
   });
 
   it("should display the movie's details", () => {
     cy.get("h4").contains("Overview");
     cy.get("h4").next().contains(movie.overview);
-    cy.get("ul")
-      .eq(1)
-      .within(() => {
-        cy.get("li").eq(0).contains("Runtime");
-        cy.get("li").eq(1).contains(movie.runtime);
-        cy.get("li").eq(2).contains("Release Date");
-        cy.get("li").eq(3).contains(movie.release_date);
-      });
+    cy.get('.list')
+        .within(() => {
+          cy.get('.content').eq(0).should("have.text", "Runtime (min.):" + movie.runtime);
+          cy.get(".content").eq(1).should("have.text", "Release Date (min.): " + movie.release_date);
+        });
   });
 
   it("should display the Home icon with the correct URL value", () => {
-    cy.get(".fa-home")
+    cy.get(".home")
       .parent()
       .should("have.attr", "href")
       .should("include", movie.homepage);
   });
 
   it("should display movie poster", () => {
-    cy.get(".movie")
+    cy.get("img")
       .should("have.attr", "src")
       .should("include", movie.poster_path);
   });
